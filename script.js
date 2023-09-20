bc133ada970c26109e86ba76aa3c68cc25f8a276
sejal173 <sejalkakade123@gmail.com>
2023-09-20 18:06:15 +0530

    added script.js file

---
+++ script.js
@@ -0,0 +1,20 @@
+function redBulb()
+{
+     const bulbElement = document.getElementById('bulb');
+     bulbElement.src="redd.png"
+} 
+function greenBulb()
+{
+     const bulbElement = document.getElementById('bulb');
+     bulbElement.src="greenn.png"
+} 
+function blueBulb()
+{
+     const bulbElement = document.getElementById('bulb');
+     bulbElement.src="bluee.png"
+} 
+function yellowBulb()
+{
+     const bulbElement = document.getElementById('bulb');
+     bulbElement.src="orangee.png"
+} 
